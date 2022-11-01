7512005ced5e382114b5adafb2b1663b2f076995
leesangwon <tkddnjs5613@naver.com>
2022-11-01 23:51:02 +0900

    feat: react-helmet meta 태그 수정

--- frontend/src/pages/Home/HomeContainer.tsx
+++ frontend/src/pages/Home/HomeContainer.tsx
@@ -10,7 +10,7 @@ const HomeContainer = () => {
         title="인스타그램 클론코딩"
         description="인스타그램을 클론코딩한 웹사이트입니다."
         keywords="클론코딩, 인스타그램"
-        url="https://d317rrnl7xcgph.cloudfront.net/home"
+        url="https://instagram-clone-sangwon.com"
         imgsrc={thumbnail}
       />
       <HomePresenter />
