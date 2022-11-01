e7c197aabfe85b3b215f898eadb6d7124b2a772a
leesangwon <tkddnjs5613@naver.com>
2022-11-02 02:51:36 +0900

    fix: 라우터 수정

--- frontend/src/AppRouter.tsx
+++ frontend/src/AppRouter.tsx
@@ -7,8 +7,7 @@ import Profile from './pages/Profile';
 
 const LoggedOutRoutes = () => (
   <Routes>
-    <Route path="*" element={<Login />} />
-    <Route path="/" element={<Login />} />
+    <Route path="/login" element={<Login />} />
     <Route path="/home" element={<Home />} />
     <Route path="/profile" element={<Profile />} />
   </Routes>
