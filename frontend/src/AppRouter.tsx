2a18255ce97bf6f966c08aa37a315bf9540410ed
leesangwon <tkddnjs5613@naver.com>
2022-11-03 03:21:45 +0900

    fix: 라우터 변경

--- frontend/src/AppRouter.tsx
+++ frontend/src/AppRouter.tsx
@@ -4,12 +4,10 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
-import SignUp from './components/feature/SignUp/SignUp';
 
 const LoggedOutRoutes = () => (
   <Routes>
     <Route path="/login" element={<Login />} />
-    <Route path="/signup" element={<SignUp />} />
     <Route path="/home" element={<Home />} />
     <Route path="/profile" element={<Profile />} />
   </Routes>
