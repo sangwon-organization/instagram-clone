e18effebf8af00108e736728ee0fce849e9d3307
suwan <blacksw1102@gmail.com>
2022-11-17 11:45:04 +0900

    modify: cors설정 수정

--- backend/middlewares/index.js
+++ backend/middlewares/index.js
@@ -6,9 +6,9 @@ const formidable = require('formidable')
 
 const corsConverter = (req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
   res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
   next()
 }
 
